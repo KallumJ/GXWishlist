7223536f3bba7033da271dde92c20e20e4bc6140
Kallum <57623845+KallumJ@users.noreply.github.com>
2022-04-24 18:33:38 +0100

    Started layout

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # GXWishlist
 
-A crossplatform game wishlisting app, for tracking games you want, and any available deals.
+A crossplatform game wishlisting app, for tracking games you want across any PC platform.
+
+### Required Envrionment Variables
+* `GXWISHLIST_SERVER_URL`: URL for [backend server](https://github.com/KallumJ/GXWishlist-Server).
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,42 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import Button from 'react-bootstrap/Button'
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">GXWishlist</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav>
+
+          <Form className="d-flex">
+            <FormControl 
+              type="search"
+              placeholder="Search game..."
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="light outline-success" onClick={ () => { searchGame() }}>Search</Button>
+          </Form>
+        </Container>
+      </Navbar>
+
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+function searchGame(): void {
+  console.log("Test")
+}
